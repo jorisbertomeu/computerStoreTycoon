23a16af2fa3e801d6b744cd0bce34e90e2051c05
Joris Bertomeu <jbertomeu@ics.fr>
2017-03-06 14:17:42 +0100

    Remove myApp

--- app/components/version/version_test.js
+++ app/components/version/version_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('CST.version module', function() {
+  beforeEach(module('CST.version'));
 
   describe('version service', function() {
     it('should return current version', inject(function(version) {
